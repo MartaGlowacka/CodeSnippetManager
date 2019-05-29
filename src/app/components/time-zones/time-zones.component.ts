c65a98a23f29cfb43d2d3ab8ad65c9a090cdd6b7
Marta <marta.glowacka@vp.pl>
2019-05-29 18:40:37 +0200

    dd

--- src/app/components/time-zones/time-zones.component.ts
+++ src/app/components/time-zones/time-zones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'time-zones',
@@ -7,20 +7,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeZonesComponent implements OnInit {
 
-  timeZones = []
+  timeZones = ['Api Token',  'Time Zones', 'Manager', 'Data Base', 'Skills', 'Employment Fields', 'Admins', 'Notes']
 
   isHover: boolean = true;
 
-  constructor() { }
+
+@HostListener('mouseenter',  ['$event'])
+onmouseover(event) {
+
+
+  let container = document.querySelector('.menu-container')
+  let ul = document.querySelector('.menu-list')
+  let li = document.querySelectorAll('li.menu-element')
+  let rect = ul.getBoundingClientRect();
+  let marginLeft = window.getComputedStyle(ul).getPropertyValue('margin-left')
+  console.log(marginLeft)
+  let slicedmargin = Number(marginLeft.substring(0, marginLeft.length - 2))
+
+
+  let pageX = event.screenX
+  let innerWidth = window.innerWidth;
+  let centerPoint = innerWidth/2
+  let outOfVieport = rect.width - innerWidth
+
+this.renderer.listen(ul, 'mouseout', () => {
+    // this.renderer.setStyle(ul, 'margin-left', 'initial')
+ })
+
+// var diff = innerWidth- rect.width;
+  // diff *= (pageX) / rect.width;//10margin of parent
+
+
+
+
+//  let diff = pageX - centerPoint + (outOfVieport - slicedmargin)
+ let firstVal = (pageX-centerPoint)*100/centerPoint
+
+ let secVal = outOfVieport + slicedmargin;
+ console.log('out',outOfVieport)
+ console.log('iiner', window.innerWidth)
+ console.log(slicedmargin)
+ console.log('to co schowane po prawej', secVal)
+
+ let result = firstVal * secVal/100
+
+ console.log(result)
+
+let diff = 0
+let allWidth = 0;
+
+
+ this.renderer.listen(ul, 'mouseover', () => {
+
+this.renderer.setStyle(ul, 'margin-left',   '-' + result + 'px')
+})
+
+
+}
+
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
-
-    for(let i=0; i<=599; i++) {
-      this.timeZones.push('TimeZ/Aaa0'+ i)
-    }
-
-
-    console.log(this.timeZones)
   }
 
 }
